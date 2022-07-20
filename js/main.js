4ef9a32b9dbb9595491d7e18ff0b6b8bf1ab8745
Ana Ragazzi <greco_al@hotmail.com>
2022-07-20 16:05:59 +0100

    feat: submit success message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const email = document.getElementById("notificaton-form__email");
 const button = document.getElementById("notification-form__button");
 const error = document.getElementById("notification-form__error-message");
+const success = document.getElementById("notification-form__success-message");
 const mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 button.addEventListener("click", (event) => {
@@ -21,14 +22,29 @@ button.addEventListener("click", (event) => {
     } else {
         clearErrorMessage();
         email.value = "";
+        success.style.display = "block";
+        success.innerHTML = "Submited";  
+        setTimeout(clearConfirmation, 3000);
     }
 });
 
 function clearErrorMessage() {
     if (mailRegex.test(email.value)) {
-        error.style.display = "block";
+        error.style.display = "none";
         email.style.border = "1px solid #c2d3ff";
         email.style.outline = "none";
-        error.style.display = "none";
     }
 }
+
+function clearConfirmation(){
+    success.style.display = "none";
+}
+
+
+
+
+        
+
+
+
+        
